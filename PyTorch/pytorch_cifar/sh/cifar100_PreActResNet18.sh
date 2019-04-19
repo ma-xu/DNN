83dc32405a28a97a42d84df2484b95553e0d47b5
13952522076 <1007540910@qq.com>
2019-04-18 20:44:14 -0500

    move sh files to new folder

--- PyTorch/pytorch_cifar/sh/cifar100_PreActResNet18.sh
+++ PyTorch/pytorch_cifar/sh/cifar100_PreActResNet18.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BATCH --job-name=PSEResNet18
+#BATCH --job-name=PreActResNet18
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=xuma@my.unt.edu
 #SBATCH --ntasks=1
@@ -19,4 +19,4 @@ module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
 module load pytorch/1.0.1
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 cifar.py --netName=PSEResNet18 --bs=512 --cifar=100
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./cifar.py --netName=PreActResNet18 --bs=512 --cifar=100
--- PyTorch/pytorch_cifar/sh/cifar100_SEResNet18.sh
+++ PyTorch/pytorch_cifar/sh/cifar100_SEResNet18.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BATCH --job-name=PreActResNet18
+#BATCH --job-name=SEResNet18
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=xuma@my.unt.edu
 #SBATCH --ntasks=1
@@ -19,4 +19,4 @@ module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
 module load pytorch/1.0.1
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 cifar.py --netName=PreActResNet18 --bs=512 --cifar=100
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 ./cifar.py --netName=SEResNet18 --bs=512 --cifar=100
