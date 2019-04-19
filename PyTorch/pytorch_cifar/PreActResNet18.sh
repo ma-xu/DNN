10f401dcb70d310f8912304f7e1ab48db732d121
13952522076 <1007540910@qq.com>
2019-04-18 20:34:33 -0500

    update sh add log folder

--- PyTorch/pytorch_cifar/PreActResNet18.sh
+++ PyTorch/pytorch_cifar/PreActResNet18.sh
@@ -10,7 +10,7 @@
 #SBATCH -c 1
 #SBATCH --gres=gpu:4
 #SBATCH -t 500:00:00
-#SBATCH --output=out_%j.log
+#SBATCH --output=outlog/out_%j.log
 module load cuda/75/blas/7.5.18
 module load cuda/75/fft/7.5.18
 module load cuda/75/nsight/7.5.18
@@ -19,4 +19,4 @@ module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
 module load pytorch/1.0.1
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 cifar.py --netName=PreActResNet18 --bs=512
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 cifar.py --netName=PreActResNet18 --bs=512 --cifar=100
