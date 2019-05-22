71f6f974c181aa99e9cdbe5547975be4204843ef
13952522076 <1007540910@qq.com>
2019-05-22 13:51:07 -0500

    vgg(original, se, sppse)

--- PyTorch/pytorch_cifar/models/se_vgg16.py
+++ PyTorch/pytorch_cifar/models/se_vgg16.py
@@ -5,21 +5,42 @@ import torch.nn.functional as F
 import math
 from torchsummary import summary
 
-__all__=['VGG16']
+__all__=['SEVGG16']
+
+class SELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SELayer, self).__init__()
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.fc = nn.Sequential(
+            nn.Linear(channel, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y = self.avg_pool(x).view(b, c)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
 class VGGBlock(nn.Module):
     def __init__(self,in_channels, channels, stride=1):
         super(VGGBlock, self).__init__()
         self.conv =  nn.Conv2d(in_channels, channels, kernel_size=3, padding=1)
         self.bn = nn.BatchNorm2d(channels)
+        self.se = SELayer(channels)
 
     def forward(self, x):
         out=self.conv(x)
         out = F.relu(self.bn(out))
+        out = self.se(out)
         return out
 
-class VGG16(nn.Module):
+class SEVGG16(nn.Module):
     def __init__(self, num_classes=100,init_weights=True):
-        super(VGG16, self).__init__()
+        super(SEVGG16, self).__init__()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.vggblock1 = self._make_layer(3,64,2)
         self.vggblock2 = self._make_layer(64, 128, 2)
@@ -51,7 +72,8 @@ class VGG16(nn.Module):
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+                if hasattr(m,'bias.data'):
+                    m.bias.data.zero_()
 
     def forward(self, x):
         out = self.vggblock1(x)
@@ -70,7 +92,7 @@ class VGG16(nn.Module):
 
 
 def test():
-    net = VGG16(num_classes=10)
+    net = SEVGG16(num_classes=10)
     x = torch.randn(2,3,32,32)
     y = net(x)
     print(y.size())
--- PyTorch/pytorch_cifar/models/sppse_vgg16.py
+++ PyTorch/pytorch_cifar/models/sppse_vgg16.py
@@ -5,21 +5,47 @@ import torch.nn.functional as F
 import math
 from torchsummary import summary
 
-__all__=['VGG16']
+__all__=['SPPSEVGG16']
+
+class SPPSELayer(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(SPPSELayer, self).__init__()
+        self.avg_pool1 = nn.AdaptiveAvgPool2d(1)
+        self.avg_pool2 = nn.AdaptiveAvgPool2d(2)
+        self.avg_pool4 = nn.AdaptiveAvgPool2d(4)
+        self.fc = nn.Sequential(
+            nn.Linear(channel*21, channel // reduction, bias=False),
+            nn.ReLU(inplace=True),
+            nn.Linear(channel // reduction, channel, bias=False),
+            nn.Sigmoid()
+        )
+
+    def forward(self, x):
+        b, c, _, _ = x.size()
+        y1 = self.avg_pool1(x).view(b, c)  # like resize() in numpy
+        y2 = self.avg_pool2(x).view(b, 4 * c)
+        y3 = self.avg_pool4(x).view(b, 16 * c)
+        y = torch.cat((y1, y2, y3), 1)
+        y = self.fc(y).view(b, c, 1, 1)
+        return x * y.expand_as(x)
+
+
 class VGGBlock(nn.Module):
     def __init__(self,in_channels, channels, stride=1):
         super(VGGBlock, self).__init__()
         self.conv =  nn.Conv2d(in_channels, channels, kernel_size=3, padding=1)
         self.bn = nn.BatchNorm2d(channels)
+        self.se = SPPSELayer(channels)
 
     def forward(self, x):
         out=self.conv(x)
         out = F.relu(self.bn(out))
+        out = self.se(out)
         return out
 
-class VGG16(nn.Module):
+class SPPSEVGG16(nn.Module):
     def __init__(self, num_classes=100,init_weights=True):
-        super(VGG16, self).__init__()
+        super(SPPSEVGG16, self).__init__()
         self.maxpool = nn.MaxPool2d(kernel_size=2, stride=2)
         self.vggblock1 = self._make_layer(3,64,2)
         self.vggblock2 = self._make_layer(64, 128, 2)
@@ -51,7 +77,8 @@ class VGG16(nn.Module):
                 m.bias.data.zero_()
             elif isinstance(m, nn.Linear):
                 m.weight.data.normal_(0, 0.01)
-                m.bias.data.zero_()
+                if hasattr(m,'bias.data'):
+                    m.bias.data.zero_()
 
     def forward(self, x):
         out = self.vggblock1(x)
@@ -70,7 +97,7 @@ class VGG16(nn.Module):
 
 
 def test():
-    net = VGG16(num_classes=10)
+    net = SPPSEVGG16(num_classes=10)
     x = torch.randn(2,3,32,32)
     y = net(x)
     print(y.size())
