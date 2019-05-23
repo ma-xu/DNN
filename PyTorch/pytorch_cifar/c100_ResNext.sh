3ec513b91ce9afe02f697db076408f0befda27a7
13952522076 <1007540910@qq.com>
2019-05-22 23:35:29 -0500

    512 to 256

--- PyTorch/pytorch_cifar/c100_ResNext.sh
+++ PyTorch/pytorch_cifar/c100_ResNext.sh
@@ -19,4 +19,4 @@ module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
 module load pytorch/1.0.1
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/DNN/PyTorch/pytorch_cifar/cifar.py --netName=ResNeXt29 --bs=512 --cifar=100
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/DNN/PyTorch/pytorch_cifar/cifar.py --netName=ResNeXt29 --bs=256 --cifar=100
