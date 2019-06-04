561e67b6692df7325572009f1c327c2c88d57766
13952522076 <1007540910@qq.com>
2019-06-04 10:38:03 -0500

    vision and batch size

--- PyTorch/pytorch_cifar/c100_CPSPPSEResNext.sh
+++ PyTorch/pytorch_cifar/c100_CPSPPSEResNext.sh
@@ -17,6 +17,6 @@ module load cuda/75/nsight/7.5.18
 module load cuda/75/profiler/7.5.18
 module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
-module load pytorch/1.0.1
+module load pytorch/1.1.0
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/DNN/PyTorch/pytorch_cifar/cifar.py --netName=CPSPPSEResNeXt29 --bs=128 --cifar=100
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/DNN/PyTorch/pytorch_cifar/cifar.py --netName=CPSPPSEResNeXt29 --bs=512 --cifar=100
