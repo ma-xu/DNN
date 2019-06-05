75e9719ad895fef6c29aba2f40e808cb2d2da287
13952522076 <1007540910@qq.com>
2019-06-05 11:31:19 -0500

    run file

--- PyTorch/pytorch_imagenet/im_SEResNeXt29.sh
+++ PyTorch/pytorch_imagenet/im_SEResNeXt29.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#BATCH --job-name=VGG16
+#BATCH --job-name=SEResNeXt29
 #SBATCH --mail-type=END,FAIL
 #SBATCH --mail-user=xuma@my.unt.edu
 #SBATCH --ntasks=1
@@ -19,4 +19,4 @@ module load cuda/75/toolkit/7.5.18
 module load cudnn/6.0/cuda75
 module load pytorch/1.1.0
 
-CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/DNN/PyTorch/pytorch_imagenet/imagenet.py --netName=VGG16 --bs=512
+CUDA_VISIBLE_DEVICES=0,1,2,3 python3 /home/xm0036/DNN/PyTorch/pytorch_imagenet/imagenet.py --netName=SEResNeXt29 --bs=512
